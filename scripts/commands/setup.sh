1acd45a11438503de85e487eaaa1b84c94d60266
Diego García <diego.garcia@adevinta.com>
2020-08-05 13:51:32 -0400

    rancher2k8s - se modifica archivo scripts/commands/setup.sh para usar go mod

--- scripts/commands/setup.sh
+++ scripts/commands/setup.sh
@@ -10,7 +10,6 @@ echoHeader "Running dependencies script"
 set -e
 # List of tools used for testing, validation, and report generation
 tools=(
-	github.com/golang/lint/golint
     github.com/axw/gocov/gocov
 	github.com/AlekSi/gocov-xml
 	gopkg.in/alecthomas/gometalinter.v1
@@ -18,10 +17,13 @@ tools=(
     github.com/fzipp/gocyclo                             # Function cyclomatic complexity analyzer
 )
 
+echoTitle "Installing the sneaky golangci-lint"
+GO111MODULE=on mod init && go get -v github.com/golangci/golangci-lint/cmd/golangci-lint@v1.26.0
+
 echoTitle "Installing missing tools"
 # Install missed tools
 for tool in ${tools[@]}; do
-	which $(basename ${tool}) > /dev/null || go get -u -v ${tool}
+	which $(basename ${tool}) > /dev/null || GO111MODULE=off go get -u -v ${tool}
 done
 
 echoTitle "Installing linters"
