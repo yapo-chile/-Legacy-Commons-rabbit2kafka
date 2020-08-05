a6488820f1eb10984994e0e8b2d9bf324196d48e
Diego García <diego.garcia@adevinta.com>
2020-08-05 14:41:40 -0400

    rancher2k8s - fix: formato archivo main.go

--- cmd/rabbit2kafka/main.go
+++ cmd/rabbit2kafka/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
 	"github.com/Yapo/logger"
 	"github.mpi-internal.com/Yapo/rabbit2kafka/pkg/infrastructure"
 	"github.mpi-internal.com/Yapo/rabbit2kafka/pkg/interfaces"
