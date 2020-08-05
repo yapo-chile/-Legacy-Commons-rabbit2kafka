c5d8a37b7592e4605ce505e0bb4c9003f5ac457e
Diego García <diego.garcia@adevinta.com>
2020-08-05 14:14:58 -0400

    rancher2k8s - se cambia versión goland de 1.11 a 1.9 y se elimina archivo glide.lock

--- scripts/commands/setup.sh
+++ scripts/commands/setup.sh
@@ -10,6 +10,7 @@ echoHeader "Running dependencies script"
 set -e
 # List of tools used for testing, validation, and report generation
 tools=(
+    github.com/golang/lint/golint
     github.com/axw/gocov/gocov
 	github.com/AlekSi/gocov-xml
 	gopkg.in/alecthomas/gometalinter.v1
@@ -17,13 +18,11 @@ tools=(
     github.com/fzipp/gocyclo                             # Function cyclomatic complexity analyzer
 )
 
-echoTitle "Installing the sneaky golangci-lint"
-GO111MODULE=on go get -v github.com/golangci/golangci-lint/cmd/golangci-lint@v1.26.0
 
 echoTitle "Installing missing tools"
 # Install missed tools
 for tool in ${tools[@]}; do
-	which $(basename ${tool}) > /dev/null || GO111MODULE=off go get -u -v ${tool}
+	which $(basename ${tool}) > /dev/null || go get -u -v ${tool}
 done
 
 echoTitle "Installing linters"
